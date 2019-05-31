336db3061c4488993524619923c7cb11e5ec24cd
Miguel Palomera <miguel.palomera@gmail.com>
2019-05-31 13:56:14 +0000

    webpack stuff

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import {hot} from 'react-hot-loader';
 
-export default class App extends React.Component {
+class App extends React.Component {
   state = {
     count: 0,
   };
   render() {
+    const {count} = this.state;
     return (
       <div>
-        <h1> Hello World!!</h1>
-        <h2>Count: {this.state.count} </h2>
+        <h1> Hello World</h1>
+        <h2 className={count > 10 ?'warning':null}>
+          Count: {this.state.count}
+        </h2>
         <button onClick={() => this.setState(state =>({count: state.count + 1}))} > + </button>
         <button onClick={() => this.setState(state =>({count: state.count - 1}))}> - </button>
       </div>
     );
   }
 }
+
+export default hot(module)(App)
